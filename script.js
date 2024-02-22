831ae7950de0fa66185999744a4eaecc1156cae3
NiteshParab <158831334+NiteshParab@users.noreply.github.com>
2024-02-22 10:32:52 +0530

    javascript
    javascript

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 let dropDown=document.querySelectorAll(".dropDown");
-const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector(".to select");
+let inputAmt=document.querySelector(".amount");
 let options=document.getElementsByTagName("option");
+let fromCurr=document.querySelector(".from select");
+let toCurr=document.querySelector(".to select");
+let btn=document.querySelector(".btn");
+let msg=document.querySelector(".msg p")
 
 for(let select of dropDown){
   for(let currCode in countryList){
     let option=document.createElement("option");
     option.innerText=currCode;
-    option.value=countryList[currCode];
+    option.value=currCode;
     select.append(option);
   }
 
   for(let option of options){
-    if(option.parentElement.name==="from" && option.value==="US"){
+    if(option.parentElement.name==="from" && option.value==="USD"){
       option.selected="selected";
     }
-    else if(option.parentElement.name==="to" && option.value==="IN"){
+    else if(option.parentElement.name==="to" && option.value==="INR"){
       option.selected="selected";
     }
     
@@ -32,10 +35,38 @@ for(let select of dropDown){
 
 const updateFlag = (element) => {
   let currCode = element.value;
-  let newSrc = `https://flagsapi.com/${currCode}/flat/64.png`;
+  let newSrc = `https://flagsapi.com/${countryList[currCode]}/flat/64.png`;
   let img=element.parentElement.querySelector(".flag");
   img.src=newSrc
 };
+
+btn.addEventListener("click",(e)=>{
+  if(inputAmt.value=="" || inputAmt.value<=0){
+    inputAmt.value="1";
+  }
+  e.preventDefault=true;
+ 
+  getExchangeRate(inputAmt.value);
+
+});
+
+async function getExchangeRate(inputAmt){
+  url=`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+
+  let response=await fetch(url);
+  let data=await response.json();
+  // msg.classList.remove("hidden");
+
+  let rate=data[toCurr.value.toLowerCase()];
+
+  let finalAmt=inputAmt*rate;
+  
+  msg.innerText=`${inputAmt} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+}
+
+
+
+
 
 
 
